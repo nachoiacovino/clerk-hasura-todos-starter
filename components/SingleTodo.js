5ad67ec24a53a52e7f32d4a6c57e723d76cd1f85
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-05-21 11:47:24 +0200

    Modify SingleTodo starting point

--- components/SingleTodo.js
+++ components/SingleTodo.js
@@ -1,31 +1,29 @@
 import { TrashIcon } from '@heroicons/react/solid'
-import { useState } from 'react'
 
 const SingleTodo = ({ todo }) => {
-  const [completed, setCompleted] = useState(todo.completed);
-
   const deleteTodo = () => {};
+  const toggleTodo = () => {};
 
   return (
-    <li key={todo.id} className='px-6 py-4 flex justify-between'>
+    <li key={todo.id} className='flex justify-between px-6 py-4'>
       <div>
         <input
           id={todo.id}
           name='completed'
           type='checkbox'
-          checked={completed}
-          onChange={(e) => setCompleted(e.target.checked)}
-          className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded mr-3'
+          checked={todo.completed}
+          onChange={toggleTodo}
+          className='w-4 h-4 mr-3 text-blue-600 border-gray-300 rounded focus:ring-blue-500'
         />
         <label
           htmlFor={todo.id}
-          className={completed ? "line-through text-gray-400" : ""}
+          className={todo.completed ? "line-through text-gray-400" : ""}
         >
           {todo.title}
         </label>
       </div>
       <TrashIcon
-        className='h-5 w-5 text-gray-500 cursor-pointer'
+        className='w-5 h-5 text-gray-500 cursor-pointer'
         onClick={deleteTodo}
       />
     </li>
